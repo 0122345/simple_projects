dabaeb40d19d7f2bb0b86c011c6c140a44ef06f3
0112345 <ntwarifiacre043@gmail.com>
2023-09-25 00:39:35 +0200

    completion of clock project

--- clock.js
+++ clock.js
@@ -25,16 +25,27 @@
         const time = new Date();
         const month = time.getMonth()
         const  day = time.getDay()
+        const  date = time.getDate()
         const  hours = time.getHours()
         const hoursForClock = hours % 12
-        const minute = time.getMinutes()
+        const minutes = time.getMinutes()
         const seconds = time.getSeconds()
+
+        const am = hours >= 12 ? 'PM' : 'AM'
         
         hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+        minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale( minutes, 0, 59, 0, 360)}deg)`
+        secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+        timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${am}`
+
+        dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
     }
     
-    const scale = (num, in_min, in_max, out_min, out_max) => {
-        return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    function scale (number, inMin, inMax, outMin, outMax) {
+        return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
     setTime()
+
+    setInterval(setTime, 1000)
