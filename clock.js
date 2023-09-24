2233e918b2495f13b97f5d32b0261ae64f072d18
0112345 <ntwarifiacre043@gmail.com>
2023-09-24 23:53:06 +0200

    dark mode and setting time

---
+++ clock.js
@@ -0,0 +1,40 @@
+    const hourEl = document.querySelector('.hr')
+    const minuteEl = document.querySelector('.minute')
+    const secondEl = document.querySelector('.second')
+    const timeEl = document.querySelector('.time')
+    const dateEl = document.querySelector('.date')
+    const toggle = document.querySelector('.toggle')
+
+
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday"];
+    const months = ["Jan","Feb"," Mar","Apr","May","Jun", "Jul", "Aug","Sep","Oct","Nov","Dec"];
+
+
+    toggle.addEventListener('click', (e)=> {
+        const html = document.querySelector('html')
+        if(html.classList.contains('dark')){
+            html.classList.remove('dark')
+            e.target.innerHTML = 'Dark mode'
+        } else{
+            html.classList.add('dark')
+            e.target.innerHTML = 'Light mode'
+        }
+    })
+
+    function setTime() {
+        const time = new Date();
+        const month = time.getMonth()
+        const  day = time.getDay()
+        const  hours = time.getHours()
+        const hoursForClock = hours % 12
+        const minute = time.getMinutes()
+        const seconds = time.getSeconds()
+        
+        hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    }
+    
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    setTime()
