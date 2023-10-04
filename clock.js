3f14e94e4bcf3c41743b778e7f1ed0cfea44a0e4
0122345 <ntwarifiacre043@gmail.com>
2023-10-05 00:40:18 +0200

    correcting some errors

--- clock.js
+++ clock.js
@@ -19,22 +19,27 @@
             html.classList.add('dark')
             e.target.innerHTML = 'Light mode'
         }
-    })
+    });
 
+
+    
     function setTime() {
         const time = new Date();
-        const month = time.getMonth()
-        const  day = time.getDay()
-        const  hours = time.getHours()
-        const hoursForClock = hours % 12
-        const minute = time.getMinutes()
-        const seconds = time.getSeconds()
+        const month = time.getMonth();
+        const  day = time.getDay();
+        const  hour = time.getHours();
+        const hoursForClock = hours % 12;
+        const minute = time.getMinutes();
+        const seconds= time.getSeconds();
         
         hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+         minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 59, 0, 360)}deg)`
+         secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 59, 0, 360)}deg)`
     }
     
     const scale = (num, in_min, in_max, out_min, out_max) => {
         return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
-    setTime()
+   
+    setTime();
