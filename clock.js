0edfffd7e668b6ac9731c5e1b0c989dfebd93c03
Ntwari Fiacre <118376302+0122345@users.noreply.github.com>
2023-10-10 02:21:06 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -26,6 +26,7 @@
         const time = new Date();
         const month = time.getMonth();
         const  day = time.getDay();
+        const date = time.getDate();
         const  hour = time.getHours();
         const hoursForClock = hour % 12;
         const minute = time.getMinutes();
@@ -36,6 +37,7 @@
          secondEl.style.transform = `translate(-50%, -100%) rotate(${scale( seconds, 0, 59, 0, 360)}deg)`
 
          timeEl.innerHTML = `${hoursForClock}:${minute < 10 ? `0${minute}` : minute}`  
+         dateEl.innerHTML = `${days[day]}, ${months[month]} `<span class = "circle">${date}</span>``
     }
     
     const scale = (num, in_min, in_max, out_min, out_max) => {
