b67e30ba9e0c977ec439a769a860d796896ed4b1
0122345 <ntwarifiacre043@gmail.com>
2023-11-21 20:25:22 +0200

    add a new project

---
+++ feedback.js
@@ -0,0 +1,11 @@
+const rating =  document.querySelectorAll('.rating');
+const senfBtn =  document.querySelectorAll('#send');
+const  panel =  document.querySelectorAll('#panel');
+
+panel.addEventListener('click', (e) => {
+   
+  console.log(e.target)
+
+});
+
+
